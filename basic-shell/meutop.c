199d9d16a70a51e7cab34f1e7ccd1def9aac33ed
GGuedesAB <gustavoguedesab@gmail.com>
2020-03-16 22:29:55 -0300

    Using ncurses to create a terminal line

--- basic-shell/meutop.c
+++ basic-shell/meutop.c
@@ -5,16 +5,19 @@
 #include <string.h>
 #include <assert.h>
 #include <dirent.h>
+#include <ncurses.h>
 #include <pwd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+#define PROC_NAME_MAX_LENGTH 25
+
 typedef struct process_table_element {
     int  pid;
     int  uid;
     char user [15];
-    char procname [15];
+    char procname [PROC_NAME_MAX_LENGTH];
     char state;
     struct process_table_element *next;
 }proctb_el;
@@ -25,7 +28,7 @@ typedef struct process_table {
 }proctb;
 
 typedef struct directory_name_element {
-    char   dirname [10];
+    char   dirname [32];
     struct directory_name_element *next;
 }dirname_el;
 
@@ -34,18 +37,37 @@ typedef struct directory_name_list {
     dirname_el *tail;
 }dirname_list;
 
-void print_process_table (const proctb *proc_tb){
-    fprintf(stdout, "PID    | User          | PROCNAME      | Estado |\n");
-    fprintf(stdout, "-------|---------------|---------------|--------|\n");
+int count_processes(const proctb *);
+
+char* process_table_to_string (const proctb *proc_tb){
+    int lines = 3;
+    int line_width = 61;
+    lines += count_processes(proc_tb);
+    char* process_table_as_string = (char*)malloc(lines*line_width*sizeof(char));
+    sprintf(process_table_as_string, "PID    | User          | PROCNAME                | Estado |\n");
+    sprintf(process_table_as_string, "%s-------|---------------|-------------------------|--------|\n", process_table_as_string);
     if (NULL == proc_tb)
-        return;
+        return NULL;
     proctb_el *current_proc_element = proc_tb->head;
     while (NULL != current_proc_element){
-        fprintf(stdout, "%7d| %14s| %14s| %c      |\n", current_proc_element->pid, current_proc_element->user, 
-                                                        current_proc_element->procname, current_proc_element->state);
+        sprintf(process_table_as_string, "%s%7d| %14s| %24s| %c      |\n", process_table_as_string, current_proc_element->pid, current_proc_element->user, 
+                                                                           current_proc_element->procname, current_proc_element->state);
         current_proc_element = current_proc_element->next;
     }
-    fprintf(stdout, "-------|---------------|---------------|--------|\n");
+    sprintf(process_table_as_string, "%s-------|---------------|-------------------------|--------|\n", process_table_as_string);
+    return process_table_as_string;
+}
+
+int count_processes(const proctb *proc_tb){
+    if (NULL == proc_tb)
+        return 0;
+    int number_of_processes = 0;
+    proctb_el *current_proc_element = proc_tb->head;
+    while (NULL != current_proc_element){
+        number_of_processes++;
+        current_proc_element = current_proc_element->next;
+    }
+    return number_of_processes;
 }
 
 int is_a_proc_number(const char *name){
@@ -63,6 +85,7 @@ dirname_list* append_dirname_to_list (const char *dirname, dirname_list *dname_l
         fprintf(stderr, "Not enough memory.\n");
         exit(1);
     }
+    memcpy(new_dir->dirname, dirname, 32);
     if (NULL != dname_list->head){
         dname_list->tail->next = new_dir;
         dname_list->tail = new_dir;
@@ -73,16 +96,19 @@ dirname_list* append_dirname_to_list (const char *dirname, dirname_list *dname_l
         dname_list->tail = new_dir;
         dname_list->tail->next = NULL;
     }
-    memcpy(new_dir->dirname, dirname, 10);
     return dname_list;
 }
 
 void destroy_dirname_list (dirname_list *dl){
+    if (NULL == dl){
+        return;
+    }
     dirname_el *current = dl->head;
     dirname_el *next = current->next;
     // Single element list
-    if (current == next){
+    if (NULL == next){
         free(current);
+        return;
     }
     while (NULL != next){
         dl->head = next;
@@ -90,6 +116,7 @@ void destroy_dirname_list (dirname_list *dl){
         current = next;
         next = next->next;
     }
+    free(current);
 }
 
 dirname_list* create_dirname_list (const char *path){
@@ -115,7 +142,7 @@ char* concatenate_paths_with_stat(const char *path1, const char *path2){
     const int str_size1 = strlen(path1);
     const int str_size2 = strlen(path2);
     const int str_size_stat = strlen("/stat");
-    char *current_proc_path = (char*)malloc((str_size1+str_size2+str_size_stat)*sizeof(char));
+    char *current_proc_path = (char*)malloc((str_size1+str_size2+str_size_stat+1)*sizeof(char));
     if (NULL == current_proc_path){
         fprintf(stderr, "Not enough memory.\n");
         exit(1);
@@ -124,7 +151,7 @@ char* concatenate_paths_with_stat(const char *path1, const char *path2){
     return current_proc_path;
 }
 
-proctb* append_proc_to_list (proctb_el proc, proctb* proc_table){
+proctb* append_proc_to_table (proctb_el proc, proctb* proc_table){
     proctb_el *new_proc = (proctb_el*)malloc(sizeof(proctb_el));
     if (NULL == new_proc){
         fprintf(stderr, "Not enough memory.\n");
@@ -143,14 +170,19 @@ proctb* append_proc_to_list (proctb_el proc, proctb* proc_table){
     return proc_table;
 }
 
-void guess_user_from_id(int id, char *name){
+char* guess_user_from_id(int id){
     struct passwd *user_info = getpwuid(id);
-    memcpy(name, user_info->pw_name, 15);
+    char* username = user_info->pw_name;
+    return username;
 }
 
-proctb* create_proc_table (const dirname_list *dname_list) {
+proctb* create_process_table (const dirname_list *dname_list) {
     struct stat buf;
     proctb_el current_proc;
+    current_proc.next=NULL;
+    current_proc.pid=-1;
+    current_proc.state=0;
+    current_proc.uid=-1;
     proctb *process_table = (proctb*)malloc(sizeof(proctb));
     if (NULL == process_table){
         fprintf(stderr, "Not enough memory.\n");
@@ -163,26 +195,90 @@ proctb* create_proc_table (const dirname_list *dname_list) {
         char *current_proc_path = concatenate_paths_with_stat("/proc/",current->dirname);
         if (0 == stat(current_proc_path, &buf)){
             current_proc.uid = buf.st_uid;
-            guess_user_from_id(current_proc.uid, current_proc.user);
+            memcpy(current_proc.user, guess_user_from_id(current_proc.uid), 15);
             FILE* proc_stat = fopen(current_proc_path, "r");
-            // Ignores first character "(" then reads everything that is not a ")", ignores last ")"
-            fscanf(proc_stat, "%d %*c%[^)]%*c %c", &current_proc.pid, current_proc.procname, &current_proc.state);
+            // Ignores first character "(" then reads first 24 chrs that are not ")", ignores last ")"
+            fscanf(proc_stat, "%d %*c%24[^)]%*c %c", &current_proc.pid, current_proc.procname, &current_proc.state);
+            fclose(proc_stat);
         }
-        else{
-            fprintf(stderr, "Could not access %s\n", current_proc_path);
-            break;
-        }
-        process_table = append_proc_to_list (current_proc, process_table);
+        process_table = append_proc_to_table (current_proc, process_table);
         current = current->next;
+        free(current_proc_path);
     }
     return process_table;
 }
 
+void destroy_process_table (proctb *ptb){
+    if (NULL == ptb){
+        return;
+    }
+    proctb_el *current = ptb->head;
+    proctb_el *next = current->next;
+    // Single element list
+    if (NULL == next){
+        free(current);
+        return;
+    }
+    while (NULL != next){
+        ptb->head = next;
+        free(current);
+        current = next;
+        next = next->next;
+    }
+    free(current);
+}
+
 int main (int argc, char* argv[]){
-    dirname_list *dl = create_dirname_list("/proc/");
-    proctb *pt = create_proc_table(dl);
-    print_process_table(pt);
-    destroy_dirname_list(dl);
-    free(dl);
+    char buf[100] = {0}, *s = buf;
+    int ch, pid = -1, sig = -1;
+    WINDOW *w;
+
+    if ((w = initscr()) == NULL) {
+        fprintf(stderr, "Error: initscr()\n");
+        exit(EXIT_FAILURE);
+    }
+    keypad(stdscr, TRUE);
+    noecho();
+    cbreak();      // disable line-buffering
+    timeout(100);  // wait 100 milliseconds for input
+
+    while (pid != 0) {
+        erase();
+        dirname_list *dl = create_dirname_list("/proc/");
+        proctb *pt = create_process_table(dl);
+        char* process_table = process_table_to_string(pt);
+        mvprintw(0, 0, "%s", process_table);
+        mvprintw(LINES-2, 0, "PID:%d SIG:%d", pid, sig);
+        mvprintw(LINES-1, 0, ">: %s", buf);
+        free(process_table);
+        destroy_dirname_list(dl);
+        free(dl);
+        destroy_process_table(pt);
+        free(pt);
+        refresh();
+        // getch (with cbreak and timeout as above)
+        // waits 100ms and returns ERR if it doesn't read anything.
+        if ((ch = getch()) != ERR) {
+            if (ch == '\n') {
+                *s = 0;
+                sscanf(buf, "%d %d", &pid, &sig);
+                kill(pid, sig);
+                s = buf;
+                *s = 0;
+            }
+            else if (ch == KEY_BACKSPACE) {
+                if (s > buf)
+                    *--s = 0;
+            }
+            else if (s - buf < (long)sizeof buf - 1) {
+                *s++ = ch;
+                *s = 0;
+            }
+        }
+    }
+
+    delwin(w);
+    endwin();
+    //printf("%s", process_table);
     return 0;
 }
